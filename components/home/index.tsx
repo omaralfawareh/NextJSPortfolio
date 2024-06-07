b7a9609c8cbc01cbfb5c536b7eba8f9ce37e2d4b
Omar <alfawareho@gmail.com>
2024-06-08 01:43:54 +0300

    home page ssr fix

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -7,69 +7,61 @@ import {
 } from "@ant-design/icons";
 import { Flex } from "antd";
 import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 
 const Home = () => {
   const router = useRouter();
   return (
-    <motion.div
-      initial={{ x: -200, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: 200, opacity: 0 }}
-      transition={{ duration: 0.8 }}
-    >
-      <div className="flex-row content-center justify-center">
-        <div className="flex-row content-center justify-center">
-          <h1>Omar Alfawareh</h1>
-          <TypeAnimation
-            className="animation"
-            sequence={[
-              "Software Engineer",
-              1000,
-              "Web Developer",
-              1000,
-              "Mobile Developer",
-              1000,
-              "Full-Stack Developer",
-              1000,
-            ]}
-            wrapper="span"
-            speed={50}
-            repeat={Infinity}
-          />
-          <p>Hardworking software engineering student and web developer.</p>
-          <Flex justify="start" gap={"middle"}>
-            <a
-              className="socials"
-              href="https://www.linkedin.com/in/omaralfawareh/"
-            >
-              <LinkedinFilled className="icons" />
-            </a>
-            <a className="socials" href="https://github.com/omaralfawareh">
-              <GithubFilled className="icons" />
-            </a>
-            <a
-              className="socials"
-              href="https://www.instagram.com/omar_alfawareh/"
-            >
-              <InstagramFilled className="icons" />
-            </a>
-            <a className="socials" href="/404">
-              <FacebookFilled className="icons" />
-            </a>
-          </Flex>
-          <button
-            onClick={() => {
-              router.push("/about");
-            }}
-            className="home-button"
+    <div className="flex-row content-center justify-center">
+      <div className="flex-row content-center justify-center animate-slide-in">
+        <h1>Omar Alfawareh</h1>
+        <TypeAnimation
+          className="animation"
+          sequence={[
+            "Software Engineer",
+            1000,
+            "Web Developer",
+            1000,
+            "Mobile Developer",
+            1000,
+            "Full-Stack Developer",
+            1000,
+          ]}
+          wrapper="span"
+          speed={50}
+          repeat={Infinity}
+        />
+        <p>Hardworking software engineering student and web developer.</p>
+        <Flex justify="start" gap={"middle"}>
+          <a
+            className="socials"
+            href="https://www.linkedin.com/in/omaralfawareh/"
           >
-            <span>About Me</span>
-          </button>
-        </div>
+            <LinkedinFilled className="icons" />
+          </a>
+          <a className="socials" href="https://github.com/omaralfawareh">
+            <GithubFilled className="icons" />
+          </a>
+          <a
+            className="socials"
+            href="https://www.instagram.com/omar_alfawareh/"
+          >
+            <InstagramFilled className="icons" />
+          </a>
+          <a className="socials" href="/404">
+            <FacebookFilled className="icons" />
+          </a>
+        </Flex>
+        <button
+          onClick={() => {
+            router.push("/about");
+          }}
+          className="home-button"
+        >
+          <span>About Me</span>
+        </button>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Sider
-        className="sider"
+        className="sider bg-white"
         breakpoint="lg"
         collapsible
         onCollapse={toggleSider}
@@ -28,10 +28,10 @@ export default function App({ Component, pageProps }: AppProps) {
         width={390}
         // trigger={<SidebarTrigger />} //for a custom sidebar trigger
       >
-        <div className="flex flex-row items-center justify-center h-full p-0">
+        <div className="flex flex-row items-center justify-center h-full p-0 dark:bg-black dark:text-white">
           <div className="flex flex-col items-center justify-center w-full">
             <Menu
-              className="menu"
+              className="menu dark:bg-black"
               theme="dark"
               mode="inline"
               onClick={({ key }) => {
@@ -64,7 +64,7 @@ export default function App({ Component, pageProps }: AppProps) {
         </div>
       </Sider>
       <Layout>
-        <Content className="flex-col justify-center items-center h-full bg-[#36454f]">
+        <Content className="flex-col justify-center items-center h-full bg-[#36454f] dark:text-white">
           <Component {...pageProps} />
         </Content>
       </Layout>
