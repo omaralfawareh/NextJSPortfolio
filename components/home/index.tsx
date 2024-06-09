1f28b132e6b266db2402eba32b83df236838f2cb
Omar <alfawareho@gmail.com>
2024-06-10 02:26:41 +0300

    fix home styling

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,66 +1,67 @@
 import React from "react";
+import Link from "next/link";
 import {
   LinkedinFilled,
   GithubFilled,
   InstagramFilled,
   FacebookFilled,
 } from "@ant-design/icons";
-import { Flex } from "antd";
 import { TypeAnimation } from "react-type-animation";
 import { useRouter } from "next/navigation";
 
 const Home = () => {
   const router = useRouter();
   return (
-    <div className="flex-row content-center justify-center">
-      <div className="flex-row content-center justify-center animate-slide-in">
-        <h1>Omar Alfawareh</h1>
-        <TypeAnimation
-          className="animation"
-          sequence={[
-            "Software Engineer",
-            1000,
-            "Web Developer",
-            1000,
-            "Mobile Developer",
-            1000,
-            "Full-Stack Developer",
-            1000,
-          ]}
-          wrapper="span"
-          speed={50}
-          repeat={Infinity}
-        />
-        <p>Hardworking software engineering student and web developer.</p>
-        <Flex justify="start" gap={"middle"}>
-          <a
-            className="socials"
-            href="https://www.linkedin.com/in/omaralfawareh/"
-          >
-            <LinkedinFilled className="icons" />
-          </a>
-          <a className="socials" href="https://github.com/omaralfawareh">
-            <GithubFilled className="icons" />
-          </a>
-          <a
-            className="socials"
-            href="https://www.instagram.com/omar_alfawareh/"
-          >
-            <InstagramFilled className="icons" />
-          </a>
-          <a className="socials" href="/404">
-            <FacebookFilled className="icons" />
-          </a>
-        </Flex>
-        <button
-          onClick={() => {
-            router.push("/about");
-          }}
-          className="home-button"
+    <div className="flex flex-col gap-2 content-center justify-center">
+      <h1 className="font-medium text-6xl">Omar Alfawareh</h1>
+      <TypeAnimation
+        className="text-xl"
+        sequence={[
+          "Software Engineer",
+          1000,
+          "Web Developer",
+          1000,
+          "Mobile Developer",
+          1000,
+          "Full-Stack Developer",
+          1000,
+        ]}
+        wrapper="span"
+        speed={50}
+        repeat={Infinity}
+      />
+      <p className="text-base text-[#bbb]">
+        Hardworking software engineering student and web developer.
+      </p>
+      <div className="flex flex-row gap-5">
+        <a
+          className="text-2xl hover:scale-150 transition"
+          href="https://www.linkedin.com/in/omaralfawareh/"
         >
-          <span>About Me</span>
-        </button>
+          <LinkedinFilled />
+        </a>
+        <a
+          className="text-2xl hover:scale-150 transition"
+          href="https://github.com/omaralfawareh"
+        >
+          <GithubFilled />
+        </a>
+        <a
+          className="text-2xl hover:scale-150 transition"
+          href="https://www.instagram.com/omar_alfawareh/"
+        >
+          <InstagramFilled />
+        </a>
+        <a className="text-2xl hover:scale-150 transition" href="/404">
+          <FacebookFilled />
+        </a>
       </div>
+      <Link
+        href="/about"
+        className="text-base bg-black px-5 py-2 w-1/3 rounded mt-2 hover:text-white text-center"
+      >
+        About Me
+      </Link>
     </div>
   );
 };
