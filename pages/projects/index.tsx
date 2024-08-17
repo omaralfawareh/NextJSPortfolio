c12eccf5a328bf5c9bfde04bda834910400f1d1f
Omar Alfawareh <alfawareho@gmail.com>
2024-08-18 00:02:28 +0300

    fix translation error on projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -32,7 +32,7 @@ const Index = () => {
   ];
   return (
     <div className="flex flex-col gap-8 min-h-screen p-1 md:p-12  text-base max-h-screen">
-      <h1 className={`${align} font-medium text-4xl`}>Projects</h1>
+      <h1 className={`${align} font-medium text-4xl`}>{t("projects")}</h1>
       <div className="flex flex-col md:flex-row gap-10 flex-wrap justify-center w-full p-3">
         {projects.map(({ text, link, src }) => (
           <Project key={link} text={text} link={link} src={src} />
